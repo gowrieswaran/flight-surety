0e48220a81069212a12ac2bb2c2a04c469e6910d
gowrieswaran <gowrieswar16@gmail.com>
2019-06-27 13:33:13 +0530

    updated package

--- truffle.js
+++ truffle.js
@@ -1,5 +1,6 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+var mnemonic =
+  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
@@ -7,8 +8,7 @@ module.exports = {
       provider: function() {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
-      network_id: '*',
-      gas: 9999999
+      network_id: "*"
     }
   },
   compilers: {
